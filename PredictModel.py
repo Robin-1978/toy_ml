7a4d253d3728c2c17e9cffd2e2c4936b2b241683
jalsy <jalsy@msn.com>
2024-08-16 00:57:53 +0800

    up

---
+++ PredictModel.py
@@ -0,0 +1,17 @@
+import DataModel
+import SSQModel
+import torch
+from torch.utils.data import DataLoader, TensorDataset
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+import os
+
+def PredictBall(model, inputs):
+    with torch.no_grad():
+        output = model(inputs)
+        predicted_numbers = (torch.topk(torch.softmax(output, dim=1), 3).indices + 1).reshape(-1)
+    
+    print(f"Predicted: {predicted_numbers.tolist()}\n")
+    return predicted_numbers
+
+def PredictSSQ():
+    pass
